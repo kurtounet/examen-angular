af86c6c35b93ac58041b7ca1b6fb975184b2a540
Anthony bossut <bossut.anthony@gmail.com>
2024-08-08 16:35:49 +0200

    upadate

--- examen-angular/src/app/card/card.component.ts
+++ examen-angular/src/app/card/card.component.ts
@@ -12,29 +12,38 @@ import { ProduitService } from '../../shared/produit.service';
 export class CardComponent {
   @Input() product!:Ifruit;
   @Input() index?:number;
-  
+  // injecter le service
   produitsService = inject(ProduitService);
 
   quantite: number = 0;
   addToCart(product: Ifruit) {
-    this.produitsService.addToCart(product);
+    //chercher si le produit existe dans le panier
+    const item = this.produitsService.cart.find((item) => item.id === product.id);
+    // si le produit existe, on ajoute la quantite
+    if (item) {
+      item.quantite += this.quantite;
+    } else {
+      // sinon, on ajoute le produit au panier
+      const productToAdd = { ...product, quantite: this.quantite };
+      this.produitsService.addToCart(productToAdd);
+    }  
+    // on remet la quantite a 0
     this.quantite = 0;
   }
+   
  
   incrementQuantity() {
+    // increment de la quantite
     this.quantite++
-    this.product.quantite=this.quantite;     
-    console.log(this.product?.quantite);       
-
-    }
-  decrementQuantity() {     
+    this.product.quantite=this.quantite; 
+}
+  decrementQuantity() { 
+    // décrement de la quantite   
     if (this.quantite > 0) {
        this.quantite--;
       this.product.quantite=this.quantite;
-    }
-     console.log(this.product?.quantite);
+    }      
   }
-  
   
 }
   
--- examen-angular/src/app/product-list/product-list.component.ts
+++ examen-angular/src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
-import { Ifruit} from '../../shared/entities';
+import { Component, EventEmitter, inject, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Ifruit } from '../../shared/entities';
 import { ProduitService } from '../../shared/produit.service';
 import { CaltaxPipe } from '../../shared/pipes/caltax.pipe';
 
@@ -9,37 +9,48 @@ import { CaltaxPipe } from '../../shared/pipes/caltax.pipe';
   standalone: true,
   imports: [CommonModule, CaltaxPipe],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrls: ['./product-list.component.css']  
 })
 export class ProductListComponent implements OnInit {
 
- @Input() inProduct: Ifruit[] = [];
- @Output() outProduit: EventEmitter<number> = new EventEmitter();
-
+  @Input() inProduct: Ifruit[] = [];
+  @Output() outProduit: EventEmitter<number> = new EventEmitter();
+// injecte le service
   produitsService = inject(ProduitService);
   cart: Ifruit[] = [];
-  ngOnInit(): void { 
-   this.cart =this.produitsService.getCart(); 
+
+ 
+  ngOnInit(): void {
+    // récupere le panier
+    this.cart = this.produitsService.getCart();
   }
+
+   
+
   removeItem(index: number): void {
+    // supprime le produit dans le panier a la position index
     this.cart.splice(index, 1);
   }
-  getTotalQuantity(): number {     
+
+  getTotalQuantity(): number {
+    // additionne la quantite de chaque produit
     return this.cart.reduce((total, item) => total + item.quantite, 0);
   }
 
-  getTotalPrixHT(){
-   
-   return this.cart.reduce((total, item) => total + item.prixHT, 0);
+  getTotalPrixHT() {    
+    // additionne le prix HT de chaque produit 
+     return this.cart.reduce((total, item) => total + (item.prixHT * item.quantite), 0);
   }
 
-  getTotalPrixTTC(){
-    let totalHT = this.cart.reduce((total, item) => total + item.prixHT, 0)
-    let ttc = (totalHT*0.2)+totalHT ;
-    
-     return ttc;
+  getTotalPrixTTC() {
+    // additionne le prix HT de chaque produit
+    let totalHT = this.cart.reduce((total, item) => total + (item.prixHT * item.quantite), 0);
+    // calcule le prix TTC
+    let ttc = (totalHT * 0.2) + totalHT;
+    return ttc;
   }
-   clearCart(): void {
-   this.cart = this.produitsService.clearCart();
+// remet le panier a 0
+  clearCart(): void {
+    this.cart = this.produitsService.clearCart();
   }
 }
